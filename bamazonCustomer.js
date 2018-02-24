59701d26a4ce5559cdad0420de5d061873c42f4c
Frank Valdez <fvaldez421@gmail.com>
2018-02-23 23:43:47 -0800

    Bamazon 2.0 with managerial functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var clc = require("cli-color");
+var Table = require("cli-table");
 
 
 var connection = mysql.createConnection({
@@ -10,13 +11,15 @@ var connection = mysql.createConnection({
 
   user: "root",
 
-  password: "CMMAnum_1",
+  password: "",
   database: "bamazonDB"
 });
 
 var username;
-var manager = false;
-var customer = false;
+var manView = false;
+var allProds = false;
+var lowInv = false;
+var noItem = false;
 
 
 connection.connect(function(err) {
@@ -25,13 +28,23 @@ connection.connect(function(err) {
 	welcomeScreen();	
 });
 
+function valStr(str) {
+	return str !== "" || "Entry must be in letters!";
+};
+
+function valNum(num) {
+	var reg = /^\d+$/;
+	return reg.test(num) || "Entry should be a number!";
+};
+
 function welcomeScreen() {
-	console.log("Welcome to Bamazon! Please Log in to continue.")
+	console.log("Welcome to Bamazon! Please Log in to continue.");
 	inquirer.prompt([
 		{
 			type: "input",
 			message: "Username: ",
-			name: "username"
+			name: "username",
+			validate: valStr
 		},
 		{
 			type: "input",
@@ -49,11 +62,10 @@ function welcomeScreen() {
 			if (input.loginType === "Manager") {
 				console.log("Welcome Manager, " + username + "!");
 				launchManager(username);
-				manager = true;
+				manView = true;
 			}else {
 				console.log("Welcome, " + username + "!")
 				launchStore(username);
-				customer = true;
 			};
 		});
 };
@@ -68,20 +80,15 @@ function launchStore(username) {
 		}
 		]).then(function(input) {
 			if (input.shopStyle === "All") {
-				console.log("");
-				shopAll();
+				showAll();
 			}else {
-				console.log("");
 				byDept();
 			};
 		});
 };
 
-function launchManager(username) {
-	console.log("INCOMPLETE FUNCTION, CHECK IN LATER :)");
-};
-function shopAll() {
-	console.log("All items listed here...");
+function showAll() {
+	console.log("");
 	connection.query("SELECT * FROM items", function(err, res) {
 		if (err) {
 			console.log(err);
@@ -94,60 +101,83 @@ function shopAll() {
 };
 
 function byDept() {
-	inquirer.prompt([
-		{
-			type: "list",
-			message: "Departments: ",
-			choices: ["appliances", "cell phone accessories", "entertainment", "hygene", "outdoor accessories"],
-			name: "deptSelect"
-		}
-		]).then(function(input) {
-			console.log(clc.green.bold("\nSelected Department: " + input.deptSelect + "\n"));
+	console.log("");
+	connection.query("SELECT * FROM items", function(err, res) {
+		if (err) throw err;
 
-			var query = connection.query(
-				"SELECT * FROM items WHERE ?", {department: input.deptSelect}, function(err, res) {
-				if (err) {
-					console.log(err);
-				}else {
+		inquirer.prompt([
+			{
+				type: "list",
+				message: "Departments: ",
+				choices: function() {
+					var choiceArray = [];
 					for (var i = 0; i < res.length; i++) {
-						console.log(res[i].id + " " + res[i].name + ": $" + res[i].price + "\n");
+						if (choiceArray.includes(res[i].department) === false) {
+							choiceArray.push(res[i].department);
+						};
+					}
+					return choiceArray;
+				},
+				name: "deptSelect"
+			}
+			]).then(function(input) {
+				console.log(clc.green.bold("\nSelected Department: " + input.deptSelect + "\n"));
+
+				var query = connection.query(
+					"SELECT * FROM items WHERE ?", {department: input.deptSelect}, function(err, res) {
+					if (err) {
+						console.log(err);
+					}else {
+						for (var i = 0; i < res.length; i++) {
+							console.log(res[i].id + " " + res[i].name + ": $" + res[i].price + "\n");
+						};
 					};
-				};
-				buy(res);
+					buy(res);
+				});
 			});
-		});
+	});
 };
 
 function buy(res) {
+	console.log("");
 	inquirer.prompt([
 		{
 			type: "input",
-			message: "Enter the number of the desired product: ",
-			name: "product"
+			message: "Enter the desired product's ID number: ",
+			name: "product",
+			validate: valNum
 		},
 		{
 			type: "input",
 			message: "How many?",
-			name: "quantity"
+			name: "quantity",
+			validate: valNum
 		}
 		]).then(function(input) {
+			if (input.product === "0" || input.quantity === "0") {
+				noItem = true;
+			}
 			var total = (input.quantity * res[0].price);
 			console.log("");
-			inquirer.prompt([
-				{
-					type: "list",
-					message: clc.red("Are you sure? ") +  "Items Total:" + clc.yellow.bold(" $" + total),
-					choices: ["Yes", "No"],
-					name: "confirm"
-				}
-				]).then(function(sure) {
-					if (sure.confirm === "Yes") {
+			if (!noItem) {
+				inquirer.prompt([
+					{
+						type: "list",
+						message: clc.red("Are you sure? ") +  "Items Total:" + clc.yellow.bold(" $" + total),
+						choices: ["Yes", "No"],
+						name: "confirm"
+					}
+					]).then(function(sure) {
+						if (sure.confirm === "Yes") {
 
-						confirmPur(input);
-					}else {
-						finished();
-					};
-				});
+							confirmPur(input);
+						}else {
+							exit();
+						};
+					});
+			}else {
+				exit();
+			};
 
 			function confirmPur(input) {
 					var query = connection.query(
@@ -163,10 +193,11 @@ function buy(res) {
 
 	function updateQuant(res, input, total) {
 		if (input.quantity > res[0].quantity) {
+			console.log("");
 			console.log(clc.red.bold("This item is out of stock :("));
-			finished();
+			exit();
 		}else {
-			var newQuant = (res[0].quantity - 1);
+			var newQuant = (res[0].quantity - parseInt(input.quantity));
 			var total = (input.quantity * res[0].price);
 			var query = connection.query(
 				"UPDATE items SET ? WHERE ?", 
@@ -179,8 +210,7 @@ function buy(res) {
 				}
 				],
 				function(err, res) {
-					// console.log("Quantity updated!");
-	    			finished();
+	    			exit();
 				}
 			);
 			if (input.quantity > 1) {
@@ -191,26 +221,234 @@ function buy(res) {
 		};
 	};
 };
-function finished() {
+
+function exit(manView) {
+	console.log("");
 	inquirer.prompt([
 		{
 			type: "list",
-			message: "Continue Shopping?",
+			message: "Exit?",
 			choices: ["Yes", "No"],
 			name: "done"
 		}
 		]).then(function(input) {
-			if (input.done === "No") {
+			if (input.done === "Yes") {
 				console.log(clc.green.bold("Thank you, come again!"));
 				connection.end();
 			}else {
-				if (customer === true) {
-					console.log("");
-					launchStore(username);
-				}else {
-					console.log("");
-					launchManager(username);
-				}
+				modeSelect();
+			};
+		});
+
+};
+
+function modeSelect() {
+	if (manView) {
+		console.log("");
+		launchManager();
+	}else {
+		console.log("");
+		launchStore();
+	};
+};
+
+function launchManager(lowInv) {
+	console.log("");
+	inquirer.prompt([
+		{
+			type: "list",
+			message: "What would you like to do?",
+			choices: ["View All Products", "View Low Inventory", "Add Inventory", "Add Product", "Exit"],
+			name: "direction"
+		}
+		]).then(function(input) {
+			switch(input.direction) {
+				case "View All Products":
+				manAll();
+				break;
+
+				case "View Low Inventory":
+				manLow();
+				break;
+
+				case "Add Inventory":
+				moreItem(lowInv);
+				break;
+
+				case "Add Product":
+				addItem();
+				break;
+
+				case "Exit":
+				manView = true;
+				exit(manView);
+				break;
+			};
+		});
+};
+
+function manAll() {
+	console.log("");
+	connection.query("SELECT * FROM items", function(err, res) {
+		var table = new Table ({
+			head: ["ID", "Item", "Price", "Dept", "Quant"],
+			colWidths: [4, 20, 8, 20, 8]
+		});
+
+		if (err) {
+			console.log(err);
+		};
+		for (var i = 0; i < res.length; i++) {
+			table.push(
+				[res[i].id, res[i].name, res[i].price, res[i].department, res[i].quantity]
+			);
+		};
+		console.log(table.toString());
+		moreItem();
+	});
+};
+
+function manLow() {
+	console.log("");
+	connection.query("SELECT * FROM items", function(err, res) {
+		var table = new Table ({
+			head: ["ID", "Item", "Price", "Quant"],
+			colWidths: [4, 20, 8, 8]
+		});
+
+		if (err) {
+			console.log(err);
+		};
+		for (var i = 0; i < res.length; i++) {
+			if (res[i].quantity < 5) {
+				table.push(
+					[res[i].id, res[i].name, res[i].price, res[i].quantity]
+				);
+			};
+		};
+		console.log(table.toString());
+		moreItem();
+	});
+};
+
+function moreItem(lowInv) {
+	console.log("");
+	inquirer.prompt([
+		{
+			type: "list",
+			message: "Would you like to restock?",
+			choices: ["Yes", "No"],
+			name: "restock"
+		}
+		]).then(function(input) {
+			if (input.restock === "Yes") {
+				addParams();
+			}else {
+				launchManager();
+			};
+		});
+
+	function addParams() {
+		console.log("");
+		inquirer.prompt([
+			{
+				type: "input",
+				message: "Enter the product's ID number: ",
+				name: "product",
+				validate: valNum
+			},
+			{
+				type: "input",
+				message: "How many?",
+				name: "quantity",
+				validate: valNum
 			}
-		})
-}
+			]).then(function(input) {
+				var query = connection.query(
+					"SELECT * FROM items WHERE ?", {id: input.product}, function(err, res) {
+					if (err) {
+						console.log(err);
+					}else {
+						updateQuant(res, input);
+					};
+				});
+
+			});
+
+		function updateQuant(res, input) {
+			var newQuant = (res[0].quantity + parseInt(input.quantity));
+			var query = connection.query(
+				"UPDATE items SET ? WHERE ?", 
+				[
+				{
+					quantity: newQuant
+				},
+				{
+					id: res[0].id
+				}
+				],
+				function(err, res) {
+					launchManager();
+				}
+			);
+			if (input.quantity > 1) {
+				console.log(clc.yellow.bold("\n" + input.quantity + " " + res[0].name + "s Added!" + "\n"));
+			}else {
+				console.log(clc.yellow.bold("\n" + input.quantity + " " + res[0].name + " Added!" + "\n"));
+			};
+		};
+	};
+};
+
+function addItem() {
+	console.log("");
+	inquirer.prompt([
+		{
+			type: "input",
+			message: "Enter item name: ",
+			name: "name",
+			validate: valStr
+		},
+		{
+			type: "input",
+			message: "Enter department: ",
+			name: "department",
+			validate: valStr
+		},
+		{
+			type: "input",
+			message: "Enter price: ",
+			name: "price",
+			validate: valNum
+		},
+		{
+			type: "input",
+			message: "Enter quantity:",
+			name: "quantity",
+			validate: valNum
+		}
+		]).then(function(input) {
+			addNew(input);
+		});
+	function addNew(input) {
+		var query = connection.query(
+			"INSERT INTO items SET ?", {
+				name: input.name,
+				department: input.department.toLowerCase(),
+				price: input.price,
+				quantity: input.quantity
+			},
+			function(err) {
+				if (err) throw err;
+				launchManager();
+			}
+		);
+		if (input.quantity > 1) {
+			console.log(clc.yellow.bold("\n" + input.quantity + " " + input.name + "s Added!" + "\n"));
+		}else {
+			console.log(clc.yellow.bold("\n" + input.quantity + " " + input.name + " Added!" + "\n"));
+		};
+	};
+};
+
+
